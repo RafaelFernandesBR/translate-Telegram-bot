b86b3bb72fbf1f05a950febdbb7b6c5ecb957a50
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-04-28 22:06:24 -0300

    adiciona validação no comando trocar

--- Translate/Commands/TrocarCommand.cs
+++ Translate/Commands/TrocarCommand.cs
@@ -13,12 +13,32 @@ public class TrocarCommand : ICommandBot
         var databaseconect = new DatabaseConect(logger);
         var texto = update.Message.Text.Split(" ");
 
-        //se o tamanho do  array for igual a 3
+        // se o tamanho do array for igual a 3
         string msgSend = null;
         if (texto.Length == 3)
         {
-            var atualizado = databaseconect.Atualizar(chatId.ToString(), texto[1], texto[2]);
-            msgSend = $"Idioma origem atualizado para: {atualizado.First().idioma_selecionado_origem}\nIdioma destino atualizado para: {atualizado.First().idioma_selecionado_destino}";
+            bool valid = true;
+            int id = 0;
+            foreach (var item in texto)
+            {
+                id++;
+                if (item.Length > 2)
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+            {
+                var atualizado = databaseconect.Atualizar(chatId.ToString(), texto[1], texto[2]);
+                msgSend = $"Idioma origem atualizado para: {atualizado.First().idioma_selecionado_origem}\nIdioma destino atualizado para: {atualizado.First().idioma_selecionado_destino}";
+            }
+            else
+            {
+                logger.Error($"Algum item do texto tem mais de 2 caracteres. O texto é {texto[id]}");
+                msgSend = "Comando inválido, verifique e envie novamente.";
+            }
         }
         else
         {
